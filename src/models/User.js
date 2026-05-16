3aa367a99dc95625bded9c16bc7bfda809411a22
Satyam Singh <1ssatyamsinghs@gmail.com>
2026-05-16 08:08:36 +0530

    user model created

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+import mongoose,{Schema} from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user"
+    }
+  },
+  {
+    timestamps: true
+  }
+);    
+
+export const User = mongoose.model("User", userSchema);
