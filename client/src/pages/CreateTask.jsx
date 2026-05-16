b6a9a569f6e9e534ed36fa14698d4c10a1662568
Satyam Singh <1ssatyamsinghs@gmail.com>
2026-05-16 18:20:20 +0530

    frontend setup

---
+++ client/src/pages/CreateTask.jsx
@@ -0,0 +1,9 @@
+const CreateTask = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">CreateTask Page</h1>
+    </div>
+  );
+};
+
+export default CreateTask;
---
+++ client/src/pages/Users.jsx
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Users Page</h1>
+    </div>
+  );
+};
+
+export default Users;
