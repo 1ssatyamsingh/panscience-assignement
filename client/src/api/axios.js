61f051a0f2d1425a7af6ed808dc12d234648318c
Satyam Singh <1ssatyamsinghs@gmail.com>
2026-05-17 11:12:33 +0530

    Remove array from axios baseURL and set to production URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: ["http://localhost:8000/api/v1","https://panscience-assignement.onrender.com/api/v1"]
+  baseURL: "https://panscience-assignement.onrender.com/api/v1"
 });
 
 api.interceptors.request.use((config) => {
